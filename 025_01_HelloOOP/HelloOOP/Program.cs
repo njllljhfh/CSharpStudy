de945483d70344d69bbe266f74d3705f69fd3072
dragon <dragonax@163.com>
2024-07-16 22:52:17 +0800

    025_类的继承和类成员的访问控制

---
+++ 025_01_HelloOOP/HelloOOP/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(Car);
+            Type tb = t.BaseType;
+            Console.WriteLine(tb.FullName);  //HelloOOP.Vehicle
+            Type tTop = tb.BaseType;
+            Console.WriteLine(tTop.FullName);  //System.Object
+            Console.WriteLine($"tTop.BaseType == null吗？ --- {tTop.BaseType == null}");
+            Console.WriteLine("------------------------------");
+
+            /*
+            概念：是一个（is a）
+                - 一个子类的实例，从语义上来讲也是父类的一个实例
+                - Car 类的一个实例，从语义上说也是 Vehicle 类的一个实例
+            */
+            Car car = new Car();
+            Console.WriteLine($"car is Vehicle 吗？ --- {car is Vehicle}");
+            Console.WriteLine($"car is Object 吗？ --- {car is Object}");
+            Console.WriteLine("------------------------------");
+
+            // 可以用一个父类类型的变量 来引用一个子类类型的实例
+            Vehicle vehicle = new Car();
+            Object o1 = new Vehicle();
+            Object o2 = new Car();
+        }
+    }
+
+    class Vehicle
+    {
+
+    }
+
+    class Toy
+    {
+
+    }
+
+    // C#中，一个类只能有一个基类，但是可以有多个基接口（后面课程会讲）
+    //class Car : Vehicle, Toy
+    // 子类的访问级别不能超过父类，比如父类是 internal 级别，那么子类就不能是 public级别。
+    //     - 但是反过来可以，父类是 public 级别，子类是 internal 级别。
+    class Car : Vehicle
+    {
+
+    }
+
+    // sealed 修饰的类，不能作为基类
+    sealed class Vehicle2
+    {
+
+    }
+}
---
+++ 025_02_HelloAccess/HelloAccess/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //RaceCar raceCar = new RaceCar();
+            //raceCar.Owner = "张三";
+            //Console.WriteLine(raceCar.Owner);
+            //Console.WriteLine("---------------------------------");
+
+            //Car car = new Car();
+            Car car = new Car("Timothy");
+            Console.WriteLine(car.Owner);
+            car.ShowOwner();
+            Console.WriteLine("---------------------------------");
+        }
+    }
+
+    class Vehicle
+    {
+        //public Vehicle()
+        //{
+        //    this.Owner = "N/A";
+        //}
+
+        // 父类的实例构造器 不能被子类继承，子类要定义自己的实例构造器
+        public Vehicle(string owner)
+        {
+            this.Owner = owner;
+        }
+
+        public string Owner { get; set; }
+    }
+
+    class Car : Vehicle
+    {
+        //public Car()
+        //{
+        //    // 调用构造器Car()时，先调用父类的构造器Vehicle()，再调用Car()的构造器
+        //    this.Owner = "Car Owner";
+        //}
+
+        public Car(string owner) : base(owner)
+        {
+            this.Owner = owner;
+        }
+
+        public void ShowOwner()
+        {
+
+            Console.WriteLine($"this.Owner = {this.Owner}");
+            // base 只能访问上一级父类的对象（即没有 base.base）
+            Console.WriteLine($"base.Owner = {base.Owner}"); // this.Owner 和 base.Owner 指向内存的同一个字符串
+            // this 和 base 可以不写，直接写 Owner
+            Console.WriteLine($"Owner = {Owner}");
+        }
+    }
+
+    //class RaceCar : Car
+    //{
+
+    //}
+}
