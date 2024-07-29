abed5a7ecc294288bbba47d8ca5b6970ebf89b01
dragon <dragonax@163.com>
2024-07-29 23:28:52 +0800

    WPF: 在XAML中为对象属性赋值

---
+++ WPF/003_HappyWPF/HappyWPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HappyWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 从资源中 获取 key 为 human 的对象
+            Human? h = this.FindResource("human") as Human;
+            if (h != null)
+            {
+                //MessageBox.Show(h.Name);
+                MessageBox.Show(h.Child.Name);
+            }
+        }
+    }
+
+
+    [TypeConverter(typeof(NameToHumanTypeConverter))]
+    public class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; } 
+    }
+
+
+    // 把字符串值 转换为其 他类型的值
+    public class NameToHumanTypeConverter:TypeConverter
+    {
+        public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+        {
+            //value: 就是在 xaml的标签里 通过属性赋值 得到的值
+            string name = value.ToString();
+            Human child = new Human();
+            child.Name = name;
+            return child;
+        }
+    }
+}
---
+++ WPF/004_HappyWPF2/HappyWPF2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HappyWPF2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
